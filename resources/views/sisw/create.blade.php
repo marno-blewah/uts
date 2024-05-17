4dbb13ed77ce1b45ae9fb4f30a343f6f889cf538
marno-blewah <wahyuimamramadhan@gmail.com>
2024-05-17 14:39:54 +0700

    main

---
+++ resources/views/sisw/create.blade.php
@@ -0,0 +1,54 @@
+@extends('template')
+
+@section('content')
+<div class="row mt-5 mb-5">
+    <div class="col-lg-12 margin-tb">
+        <div class="float-left">
+            <h2>Create New Siswa</h2>
+        </div>
+        <div class="float-right">
+            <a class="btn btn-secondary" href="{{ route('sisw.index') }}"> Kembali</a>
+        </div>
+    </div>
+</div>
+
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Whoops!</strong> Input gagal.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+<form action="{{ route('sisw.store') }}" method="POST">
+    @csrf
+
+     <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>NIM:</strong>
+                <input type="text" name="NIS" class="form-control" placeholder="NIM">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Nama:</strong>
+                <input type="text" name="NamaSiswa" class="form-control" placeholder="NAMA LENGKAP">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Alamat:</strong>
+                <textarea class="form-control" style="height:150px" name="Alamat" placeholder=""></textarea>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+
+</form>
+@endsection
